60de6d6558bae795c3cafe9abce4b8cc26923ad5
Serhii Kolodiazhnyi <kolodiazhniy.s@gmail.com>
2026-09-08 23:25:22 +0700

    feat: implement interval struct and func

---
+++ internal/geom/interval.go
@@ -0,0 +1,27 @@
+package geom
+
+import "math"
+
+type Interval struct {
+	Min, Max float64
+}
+
+func NewEmptyInterval() Interval {
+	return Interval{Min: math.Inf(1), Max: math.Inf(-1)}
+}
+
+func NewUniversalInterval() Interval {
+	return Interval{Min: math.Inf(-1), Max: math.Inf(1)}
+}
+
+func (i Interval) Size() float64 {
+	return i.Max - i.Min
+}
+
+func (i Interval) Contains(x float64) bool {
+	return i.Min <= x && x <= i.Max
+}
+
+func (i Interval) Surrounds(x float64) bool {
+	return i.Min < x && x < i.Max
+}
